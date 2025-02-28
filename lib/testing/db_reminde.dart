be779932cce431ae0ed42cac8202a72e7e0cc6c2
Gelo Figueroa <54201948+GeloAndres@users.noreply.github.com>
2025-02-28 11:39:19 -0300

    impl db_testing static

---
+++ lib/testing/db_reminde.dart
@@ -0,0 +1,161 @@
+import 'package:vivebien/domain/entities/reminder.dart';
+import 'package:vivebien/enum/entity_reminder/entity_reminder_enum.dart';
+import 'package:vivebien/infrastructure/entities/reminder_impl.dart';
+
+final List<Reminder> recordatorioListaTesting = [
+  ReminderImpl(
+    title: 'Estirar espalda',
+    description: 'Acuerdate de sentarte con la espada derecha',
+    reminderTime: DateTime(2025, 10, 15, 3, 30),
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'respiraciones profudas',
+    description: 'respira profundamente durante unos minutos para relajarte',
+    reminderTime: DateTime(2025, 3, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'mover los pies',
+    description: 'no olvider caminar un poco para ayudar tu circulacion',
+    reminderTime: DateTime(2025, 11, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Articular cuello',
+    description: 'mueve el cuello para evitar el endurecimiento del cuello',
+    reminderTime: DateTime(2025, 4, 1, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Estirar espalda',
+    description: 'Acuerdate de sentarte con la espada derecha',
+    reminderTime: DateTime(2025, 10, 15, 3, 30),
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'respiraciones profudas',
+    description: 'respira profundamente durante unos minutos para relajarte',
+    reminderTime: DateTime(2025, 3, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'mover los pies',
+    description: 'no olvider caminar un poco para ayudar tu circulacion',
+    reminderTime: DateTime(2025, 11, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Articular cuello',
+    description: 'mueve el cuello para evitar el endurecimiento del cuello',
+    reminderTime: DateTime(2025, 4, 1, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Estirar espalda',
+    description: 'Acuerdate de sentarte con la espada derecha',
+    reminderTime: DateTime(2025, 10, 15, 3, 30),
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'respiraciones profudas',
+    description: 'respira profundamente durante unos minutos para relajarte',
+    reminderTime: DateTime(2025, 3, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'mover los pies',
+    description: 'no olvider caminar un poco para ayudar tu circulacion',
+    reminderTime: DateTime(2025, 11, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Articular cuello',
+    description: 'mueve el cuello para evitar el endurecimiento del cuello',
+    reminderTime: DateTime(2025, 4, 1, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Estirar espalda',
+    description: 'Acuerdate de sentarte con la espada derecha',
+    reminderTime: DateTime(2025, 10, 15, 3, 30),
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'respiraciones profudas',
+    description: 'respira profundamente durante unos minutos para relajarte',
+    reminderTime: DateTime(2025, 3, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'mover los pies',
+    description: 'no olvider caminar un poco para ayudar tu circulacion',
+    reminderTime: DateTime(2025, 11, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Articular cuello',
+    description: 'mueve el cuello para evitar el endurecimiento del cuello',
+    reminderTime: DateTime(2025, 4, 1, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Estirar espalda',
+    description: 'Acuerdate de sentarte con la espada derecha',
+    reminderTime: DateTime(2025, 10, 15, 3, 30),
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'respiraciones profudas',
+    description: 'respira profundamente durante unos minutos para relajarte',
+    reminderTime: DateTime(2025, 3, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'mover los pies',
+    description: 'no olvider caminar un poco para ayudar tu circulacion',
+    reminderTime: DateTime(2025, 11, 15, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+  ReminderImpl(
+    title: 'Articular cuello',
+    description: 'mueve el cuello para evitar el endurecimiento del cuello',
+    reminderTime: DateTime(2025, 4, 1, 3, 30),
+
+    frecuencia: Frecuencia.Diario,
+    estado: Estado.Completado,
+  ),
+];
