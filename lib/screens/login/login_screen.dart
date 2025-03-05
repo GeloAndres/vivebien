485bd368ab87ab7a57443bfa26a78913ca5c0373
Gelo Figueroa <54201948+GeloAndres@users.noreply.github.com>
2025-03-05 17:06:28 -0300

    .

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:vivebien/screens/login/logup_screen.dart';
+import 'package:vivebien/screens/home/home_screen.dart';
+import 'package:vivebien/service/auth/auth_service.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Iniciar Sesión',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Correo Electrónico',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, ingresa tu correo electrónico';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, ingresa tu contraseña';
+                  }
+                  if (value.length < 4) {
+                    return 'La contraseña debe tener al menos 4 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    print('correo: ${_emailController.text}');
+                    print('contraseña: ${_passwordController.text}');
+                    final bool validation = await AuthService().signIn(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+
+                    if (validation) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    } else {
+                      print('error inesperadicimo');
+                    }
+
+                    mostrarSnackBar(context, 'Bienvenido a ViveBien');
+                  } else {
+                    print('error inesperado');
+                  }
+                },
+                child: Text('Ingresar'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LogupScreen()),
+                  );
+                },
+                child: Text('Crear una cuenta'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
